880ce9c9b23e13002feb8f7fabd60b474046a01b
hubinab <hubinab@gmail.com>
2025-01-13 10:45:05 +0100

    3-as feladat finomítása

--- sakk2040.py
+++ sakk2040.py
@@ -1,22 +1,24 @@
-Lista = []
+SzotarLista = []
+with open("eredmenyek.txt","r",encoding="utf-8") as filebe:
+    sorok = filebe.read().splitlines()
+    for i in range(len(sorok)):
+        sor = sorok[i]
+        SzotarLista.append({"eredmeny":sorok[i]})
+
 Fekete = 0
 Feher = 0
 Dontetlen = 0
-with open("eredmenyek.txt","r",encoding="utf-8") as filebe:
-    sorok = filebe.read().splitlines()
-    for sor in sorok:
-        if sor == "FK":
+for i in range(len(SzotarLista)):
+        if SzotarLista[i]["eredmeny"] == "FK":
             Fekete += 1
-        if sor == "FH":
+        if SzotarLista[i]["eredmeny"] == "FH":
             Feher += 1
-        if sor == "XX":
+        if SzotarLista[i]["eredmeny"] == "XX":
             Dontetlen += 1
-        Lista.append(sor)
-
 
 with open("pontozotabla.txt","w",encoding="utf-8") as fileki:
     print(f"A 2040-es sakkolimpián a következő eredmények születtek:", file=fileki)
-    print(f"Az olimpián összesen {len(sorok)} partit játszottak a versenyzők egymással.", file=fileki)
+    print(f"Az olimpián összesen {len(SzotarLista)} partit játszottak a versenyzők egymással.", file=fileki)
     print(f"Ezek lebontása a következő:", file=fileki)
     print(f"Fehér színben játszva összesen {Feher} partit nyertek.", file=fileki)
     print(f"Fekete színben játszva összesen {Fekete} partit nyertek.", file=fileki)
